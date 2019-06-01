465d1d270435d565d3f8b9f90483d70b30224567
Armandres30 <armando_zax@hotmail.com>
2019-06-01 17:48:13 +0200

    Add classification code

--- streamer.py
+++ streamer.py
@@ -3,6 +3,10 @@ from tcp_pickle_stream import streamer
 import picamera
 import picamera.array
 from PIL import Image
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
 
 s = streamer("192.168.93.26")
 
@@ -11,7 +15,17 @@ with picamera.PiCamera() as camera:
 		camera.resolution = (320, 240)		
 		for frame in camera.capture_continuous(stream, format="bgr", use_video_port=True):
 			image = frame.array
-			s.send_frame(image)
+
+			//get prediction of image
+			img = image.load_img(image, target_size=(224, 224))
+			x = image.img_to_array(img)
+			x = np.expand_dims(x, axis=0)
+			x = preprocess_input(x)
+
+			preds = model.predict(x)
+			//get prediction of image
+
+			s.send_frame(preds)
 			stream.truncate(0)
 
 	
